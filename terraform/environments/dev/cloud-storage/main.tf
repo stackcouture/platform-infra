fda8fe5c67b095d94c2cd4e3bf79dea7d333ee9a
Naveen <naveenramlu28@gmail.com>
2026-05-26 13:18:39 +0530

    #update the module name

--- terraform/environments/dev/cloud-storage/main.tf
+++ terraform/environments/dev/cloud-storage/main.tf
@@ -1,5 +1,5 @@
-module "gcs" {
-  source               = "./modules/cloud-storage"
+module "cloud_storage" {
+  source               = "../../modules/cloud-storage"
   bucket_name          = var.bucket_name
   location             = var.location
   storage_class        = var.storage_class
